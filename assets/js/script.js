ea81146d176458d84c2c250387020e693a562684
Kris Ringler <kmringler@gmail.com>
2019-10-02 13:21:57 -0700

    first card flip works working on getting it to be cleaner in the flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+$(document).ready(initializeApp)
 
+function initializeApp(){
+  $('.cardContainer').on("click", handleCardClick)
+}
+
+function handleCardClick(event){
+ var clickMe = $(event.currentTarget.lastElementChild)
+ clickMe.toggleClass('hidden');
+
+ console.log(clickMe);
+}
