f1961054d47c95ce907eece09c005c0c811b76d0
Kris Ringler <kmringler@gmail.com>
2019-10-02 16:58:01 -0700

    first card clicked is semi functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
 $(document).ready(initializeApp)
 
-function initializeApp(){
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = 0;
+
+function initializeApp() {
   $('.cardContainer').on("click", handleCardClick)
 }
 
-function handleCardClick(event){
- var clickMe = $(event.currentTarget.lastElementChild)
- clickMe.toggleClass('hidden');
+function handleCardClick(event) {
+  var clickMe = $(event.currentTarget.lastElementChild)
+  clickMe.toggleClass('hidden');
 
- console.log(clickMe);
+  if (firstCardClicked === null) {
+    firstCardClicked = $(event.currentTarget);
+  } else {
+    secondCardClicked = $(event.currentTarget);
+    var firstImage = firstCardClicked.find('.cardFront').css('background-image');
+    var secondImage = secondCardClicked.find('.cardFront').css('background-image');
+    if (firstImage === secondImage) {
+      console.log('its a match!')
+      matches++
+    } else {
+
+      console.log('keep trying!')
+    }
+
+
+  }
+
+
 }
